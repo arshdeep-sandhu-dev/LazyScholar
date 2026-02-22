eaf64b3fb34a2b5ce021e0505a385a4788bd70a9
Arsh Sandhu <arsh.sandhu.dev@gmail.com>
2026-02-21 23:04:01 -0600

    Initial Commit

--- template/src/context/auth/AuthContext.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext} from "react";
-
-// const AuthContext = React.createContext();
-const AuthContext = createContext();
-
-export default AuthContext;
--- template/src/context/help/HelpContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const HelpContext = createContext();
-
-export default HelpContext;
